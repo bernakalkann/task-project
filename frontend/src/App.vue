521b63694fb8f07f78dafcefd05b3c424b1bb6a5
Berna Kalkan <brnklkn8@gmail.com>
2026-07-26 20:43:24 +0300

    feat: üst bar butonlarına (Çalışma Alanları, Projeler, Filtreler, Panolar, Arama, Yardım) açılır menüler ve fonksiyonellik eklendi

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,10 +26,26 @@
 
     <!-- Top Bar (Jira Style) -->
     <v-app-bar v-if="isLoggedIn" app color="surface" elevation="0" border class="px-3">
-      <!-- Nine Dots App Switcher -->
-      <v-btn icon variant="text" color="grey-darken-2" class="mr-1">
-        <v-icon size="24">mdi-apps</v-icon>
-      </v-btn>
+      
+      <!-- Nine Dots App Switcher Menu -->
+      <v-menu location="bottom start">
+        <template v-slot:activator="{ props }">
+          <v-btn icon variant="text" color="grey-darken-2" class="mr-1" v-bind="props" title="Uygulamalar">
+            <v-icon size="24">mdi-apps</v-icon>
+          </v-btn>
+        </template>
+        <v-card width="260" class="rounded-lg elevation-4 border pa-2">
+          <v-list density="compact" nav>
+            <v-list-subheader class="font-weight-bold text-uppercase">GoJira Uygulamaları</v-list-subheader>
+            <v-divider class="my-1"></v-divider>
+            <v-list-item prepend-icon="mdi-clipboard-text-multiple" title="Görev Panosu" @click="router.push('/tasks')"></v-list-item>
+            <v-list-item prepend-icon="mdi-chart-pie" title="Anasayfa Panosu" @click="router.push('/')"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-cog" title="Profilim" @click="router.push('/profile')"></v-list-item>
+            <v-list-item v-if="isStaff" prepend-icon="mdi-account-group" title="Kullanıcı Yönetimi" @click="router.push('/users')"></v-list-item>
+            <v-list-item v-if="isStaff" prepend-icon="mdi-shield-text-outline" title="Sistem Logları" @click="router.push('/logs')"></v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
       
       <!-- Jira Logo & Brand -->
       <div class="d-flex align-center cursor-pointer mr-6" @click="router.push('/')">
@@ -37,11 +53,76 @@
         <span class="font-weight-black text-body-1 text-grey-darken-3 tracking-wide" style="font-family: sans-serif;">GoJira</span>
       </div>
 
-      <!-- Left Links -->
-      <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-2 d-none d-lg-flex" size="small">Çalışma Alanları <v-icon size="16">mdi-chevron-down</v-icon></v-btn>
-      <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-2 d-none d-lg-flex" size="small">Projeler <v-icon size="16">mdi-chevron-down</v-icon></v-btn>
-      <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-2 d-none d-lg-flex" size="small">Filtreler <v-icon size="16">mdi-chevron-down</v-icon></v-btn>
-      <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-6 d-none d-lg-flex" size="small">Panolar <v-icon size="16">mdi-chevron-down</v-icon></v-btn>
+      <!-- Çalışma Alanları Dropdown Menu -->
+      <v-menu location="bottom start">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-2 d-none d-lg-flex" size="small" v-bind="props">
+            Çalışma Alanları <v-icon size="16" class="ml-1">mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-card width="280" class="rounded-lg elevation-4 border pa-2">
+          <v-list density="compact">
+            <v-list-subheader class="font-weight-bold">Mevcut Çalışma Alanı</v-list-subheader>
+            <v-list-item prepend-icon="mdi-briefcase-check" title="GoJira Görev Takip Alanı" subtitle="Aktif Alan" color="indigo" active></v-list-item>
+            <v-divider class="my-2"></v-divider>
+            <v-list-item prepend-icon="mdi-view-dashboard-variant-outline" title="Tüm Çalışma Alanlarını Gör" @click="router.push('/tasks')"></v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
+      <!-- Projeler Dropdown Menu -->
+      <v-menu location="bottom start">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-2 d-none d-lg-flex" size="small" v-bind="props">
+            Projeler <v-icon size="16" class="ml-1">mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-card width="280" class="rounded-lg elevation-4 border pa-2">
+          <v-list density="compact">
+            <v-list-subheader class="font-weight-bold">Son Projeler</v-list-subheader>
+            <v-list-item prepend-icon="mdi-folder-text-outline" title="MSP Task Board" subtitle="Yazılım Projesi" @click="router.push('/tasks')"></v-list-item>
+            <v-list-item prepend-icon="mdi-bug-outline" title="Sprint & Bug Tracker" subtitle="Hata Takip" @click="router.push('/tasks')"></v-list-item>
+            <v-divider class="my-2"></v-divider>
+            <v-list-item prepend-icon="mdi-format-list-bulleted" title="Tüm Projeleri İncele" @click="router.push('/tasks')"></v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
+      <!-- Filtreler Dropdown Menu -->
+      <v-menu location="bottom start">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-2 d-none d-lg-flex" size="small" v-bind="props">
+            Filtreler <v-icon size="16" class="ml-1">mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-card width="260" class="rounded-lg elevation-4 border pa-2">
+          <v-list density="compact">
+            <v-list-subheader class="font-weight-bold">Hızlı Filtreler</v-list-subheader>
+            <v-list-item prepend-icon="mdi-account-check-outline" title="Bana Atanan Görevler" @click="navigateToFilter('assigned_me')"></v-list-item>
+            <v-list-item prepend-icon="mdi-alert-decagram-outline" title="Acil (Critical) Görevler" @click="navigateToFilter('critical')"></v-list-item>
+            <v-list-item prepend-icon="mdi-bug-outline" title="Hata (Bug) Görevleri" @click="navigateToFilter('bug')"></v-list-item>
+            <v-divider class="my-2"></v-divider>
+            <v-list-item prepend-icon="mdi-filter-off-outline" title="Tüm Görevleri Gör" @click="router.push('/tasks')"></v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
+      <!-- Panolar Dropdown Menu -->
+      <v-menu location="bottom start">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="text" class="text-capitalize text-grey-darken-3 font-weight-medium px-2 mr-6 d-none d-lg-flex" size="small" v-bind="props">
+            Panolar <v-icon size="16" class="ml-1">mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-card width="260" class="rounded-lg elevation-4 border pa-2">
+          <v-list density="compact">
+            <v-list-subheader class="font-weight-bold">Mevcut Panolar</v-list-subheader>
+            <v-list-item prepend-icon="mdi-developer-board" title="Kanban Pano" @click="router.push('/tasks')"></v-list-item>
+            <v-list-item prepend-icon="mdi-chart-line" title="İstatistik Panosu" @click="router.push('/')"></v-list-item>
+            <v-list-item v-if="isStaff" prepend-icon="mdi-shield-text-outline" title="Admin Log Panosu" @click="router.push('/logs')"></v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
 
       <!-- Create Button -->
       <v-btn color="blue-darken-2" variant="flat" size="small" class="text-capitalize font-weight-bold px-4 mr-4" rounded="sm" @click="triggerGlobalCreate">
@@ -51,18 +132,18 @@
       <!-- Search Bar -->
       <div style="width: 220px;" class="mr-auto d-none d-lg-block">
         <v-text-field
-          placeholder="Ara..."
+          v-model="globalSearch"
+          placeholder="Görevlerde ara..."
           variant="outlined"
           density="compact"
           hide-details
           prepend-inner-icon="mdi-magnify"
           style="max-height: 40px;"
+          @keyup.enter="handleSearchSubmit"
         ></v-text-field>
       </div>
 
       <v-spacer class="d-lg-none"></v-spacer>
-
-
 
       <!-- Notification Badge & Menu -->
       <v-menu location="bottom end" :close-on-content-click="false">
@@ -93,18 +174,18 @@
         </v-card>
       </v-menu>
 
-      <!-- Help Icon -->
-      <v-btn icon variant="text" color="grey-darken-2" class="mr-1 d-none d-sm-flex">
+      <!-- Help Icon (Yardım Modalı Tetikler) -->
+      <v-btn icon variant="text" color="grey-darken-2" class="mr-1 d-none d-sm-flex" @click="helpDialog = true" title="Yardım ve Dokümantasyon">
         <v-icon size="22">mdi-help-circle-outline</v-icon>
       </v-btn>
 
-      <!-- Settings Icon -->
-      <v-btn icon variant="text" color="grey-darken-2" class="mr-1 d-none d-sm-flex">
+      <!-- Settings Icon (Profil & Ayarlara Yönlendirir) -->
+      <v-btn icon variant="text" color="grey-darken-2" class="mr-1 d-none d-sm-flex" @click="router.push('/profile')" title="Ayarlar">
         <v-icon size="22">mdi-cog-outline</v-icon>
       </v-btn>
 
       <!-- Theme Switcher -->
-      <v-btn icon variant="text" color="grey-darken-2" class="mr-2" @click="toggleTheme">
+      <v-btn icon variant="text" color="grey-darken-2" class="mr-2" @click="toggleTheme" title="Tema Değiştir">
         <v-icon size="22">{{ currentTheme === 'dark' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
       </v-btn>
 
@@ -114,7 +195,7 @@
       </v-btn>
 
       <!-- User Avatar / Initials -->
-      <v-avatar color="indigo-darken-2" size="32" class="cursor-pointer font-weight-bold elevation-1 mr-2" @click="router.push('/profile')">
+      <v-avatar color="indigo-darken-2" size="32" class="cursor-pointer font-weight-bold elevation-1 mr-2" @click="router.push('/profile')" title="Profilim">
         <v-img v-if="userAvatar" :src="userAvatar"></v-img>
         <span v-else>{{ username.substring(0, 2).toUpperCase() }}</span>
       </v-avatar>
@@ -124,6 +205,34 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <!-- YARDIM VE REHBER MODALI -->
+    <v-dialog v-model="helpDialog" max-width="550">
+      <v-card class="rounded-xl">
+        <v-card-title class="bg-indigo-darken-4 text-white py-3 px-4 font-weight-bold d-flex align-center">
+          <v-icon icon="mdi-help-circle" class="mr-2"></v-icon>
+          GoJira Kullanım Rehberi & Yardım
+        </v-card-title>
+        <v-card-text class="pa-6">
+          <v-list density="compact" border rounded class="mb-4">
+            <v-list-item prepend-icon="mdi-numeric-1-circle" title="Görev Panosu" subtitle="Kanban panosu üzerinden görev durumlarını sürükleyip bırakarak güncelleyebilirsiniz."></v-list-item>
+            <v-divider></v-divider>
+            <v-list-item prepend-icon="mdi-numeric-2-circle" title="Yeni Görev Oluştur" subtitle="Üst bardaki 'Oluştur' butonuna tıklayarak yeni görev ekleyebilirsiniz."></v-list-item>
+            <v-divider></v-divider>
+            <v-list-item prepend-icon="mdi-numeric-3-circle" title="Şifre Güvenliği" subtitle="Parolalar en az 8 karakter, rakam, sembol, büyük ve küçük harf içermelidir."></v-list-item>
+            <v-divider></v-divider>
+            <v-list-item prepend-icon="mdi-numeric-4-circle" title="OTP ile Giriş" subtitle="Giriş yaparken e-posta adresinize 6 haneli doğrulama kodu (OTP) iletilir."></v-list-item>
+            <v-divider></v-divider>
+            <v-list-item v-if="isStaff" prepend-icon="mdi-numeric-5-circle" title="Admin Logları" subtitle="Yöneticiler sol menüdeki 'Sistem Logları' ekranından tüm HTTP isteklerini inceleyebilir."></v-list-item>
+          </v-list>
+        </v-card-text>
+        <v-card-actions class="pa-4 bg-grey-lighten-4">
+          <v-spacer></v-spacer>
+          <v-btn color="indigo-darken-4" variant="flat" @click="helpDialog = false">Anladım</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-app>
 </template>
 
@@ -137,11 +246,12 @@ const route = useRoute()
 const router = useRouter()
 const theme = useTheme()
 
-const drawer = ref(true)
 const isLoggedIn = ref(false)
 const isStaff = ref(false)
 const username = ref('')
 const notifications = ref([])
+const globalSearch = ref('')
+const helpDialog = ref(false)
 
 const unreadCount = computed(() => notifications.value.filter(n => !n.is_read).length)
 const currentTheme = computed(() => theme.global.name.value)
@@ -203,7 +313,7 @@ onMounted(() => {
   const savedTheme = localStorage.getItem('theme')
   if (savedTheme) theme.global.name.value = savedTheme
   
-  // Bildirimleri her 60 saniyede bir otomatik tazele (Canlılık katar)
+  // Bildirimleri her 60 saniyede bir otomatik tazele
   setInterval(fetchNotifications, 60000)
 
   // Profil resmi güncelleme olayını dinle
@@ -229,5 +339,14 @@ const triggerGlobalCreate = () => {
   } else {
     router.push({ path: '/tasks', query: { create: 'true' } })
   }
+}
+
+const handleSearchSubmit = () => {
+  if (!globalSearch.value) return
+  router.push({ path: '/tasks', query: { search: globalSearch.value } })
+}
+
+const navigateToFilter = (filterType) => {
+  router.push({ path: '/tasks', query: { filter: filterType } })
 }
 </script>
